e6cb2c5dc29b957a6fa8451f5f61a8c560d0f33c
vijayago <palpapvijay@gmail.com>
2019-09-26 10:05:17 +0530

    Step file

---
+++ Features/Steps/Testautothon.py
@@ -0,0 +1,83 @@
+@when(u'search "step-in forum facebook"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When search "step-in forum facebook"')
+
+
+@when(u'click on results that contains text "25000 test professionals"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When click on results that contains text "25000 test professionals"')
+
+
+@when(u'Navigate to the link fetched from the above step')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When Navigate to the link fetched from the above step')
+
+
+@when(u'Navigate to Posts')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When Navigate to Posts')
+
+
+@when(u'Find out the top post that has more than 4 photos.')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When Find out the top post that has more than 4 photos.')
+
+
+@when(u'Download top 5 photos in that post')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When Download top 5 photos in that post')
+
+
+@when(u'Rename files in sequence')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When Rename files in sequence')
+
+
+@when(u'Validate size > 0 KB for each file')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When Validate size > 0 KB for each file')
+
+
+@then(u'files should be downloaded and renamed successfully')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then files should be downloaded and renamed successfully')
+
+
+@when(u'Navigate to the Photo\'s and Click See All')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When Navigate to the Photo\'s and Click See All')
+
+
+@when(u'Extract the album name and photo count for each album')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When Extract the album name and photo count for each album')
+
+
+@when(u'Format the observation in JSON Format')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When Format the observation in JSON Format')
+
+
+@when(u'Write to file named "data.json"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When Write to file named "data.json"')
+
+
+@then(u'data.json file should be created with album name and photo counts')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then data.json file should be created with album name and photo counts')
+
+
+@when(u'Navigate to the data.json file file location')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When Navigate to the data.json file file location')
+
+
+@when(u'Upload it using HTTP details')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When Upload it using HTTP details')
+
+
+@then(u'Verify file should be uploaded successfully')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then Verify file should be uploaded successfully')
